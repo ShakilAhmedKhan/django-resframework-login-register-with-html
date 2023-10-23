38e7cf9474d84a47d6d7554ae274608d0c2b6896
Shakil Ahmed Khan <shakilcust@gmail.com>
2023-10-23 23:23:58 +0600

    add

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -84,10 +84,20 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_media',
+        'USER': 'mediusware',
+        'PASSWORD': 'mediusware',
     }
 }
 

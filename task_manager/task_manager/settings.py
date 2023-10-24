840db1b2191907a7fc860f6fa34f95077b7ca9f5
Shakil Ahmed Khan <shakilcust@gmail.com>
2023-10-24 12:01:44 +0600

    add

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -110,7 +110,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIR = {
     os.path.join(BASE_DIR, "media")
 }
-
+#
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
